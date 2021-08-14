362cdc20028f62ebb3a2a4c54f82fd8cee52c46f
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-08-14 15:34:34 +0200

    first commit

---
+++ Scripts/register.js
@@ -0,0 +1,12 @@
+
+fetch('https://murmuring-everglades-76424.herokuapp.com/show-users/')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+const form = document.querySelector(".form")
+function submitForm(event){
+  event.preventDefault()
+  window.location.href = "./veiwprod.html"
+}
+form.addEventListener("submit", submitForm)
