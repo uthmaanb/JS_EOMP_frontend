22ca6e016059d3d6542585427965f167c4eb2984
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-08-16 11:15:48 +0200

    update

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,3 +1,26 @@
 fetch('https://murmuring-everglades-76424.herokuapp.com/login/')
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+
+  function admin(){
+    fetch('https://murmuring-everglades-76424.herokuapp.com/login/', {
+    method: "GET",
+    body: JSON.stringify({
+        'admin_username': document.getElementById("username").value,
+        'admin_password': document.getElementById("password").value,
+    }),
+    headers: {
+        'Content-type': 'application/json',
+    }
+    // }).then(response => response.json()).then(data => {
+    //     console.log(data)
+    //     if (data['description'] == 'Invalid credentials'){
+    //     alert("Error not valid login in!")
+    //     }
+    // else{
+    //     window.location.href = './veiwprod.html'
+    // }    
+    });
+    
+}
