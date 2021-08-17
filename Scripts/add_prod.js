32868e7abae15d484599f664cd668d38e5ab9098
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-08-17 11:58:41 +0200

    update

--- Scripts/add_prod.js
+++ Scripts/add_prod.js
@@ -1,3 +1,16 @@
-fetch('https://murmuring-everglades-76424.herokuapp.com/show-products/')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+
+// const baseURL = "https://murmuring-everglades-76424.herokuapp.com/prod-registration";
+
+const form = document.querySelector("#form");
+
+function submitForm(event) {
+	event.preventDefault();
+    if (confirm('do you want to add another product?')){
+        window.location.href = './add_prod.html'
+
+    }else {
+        window.location.href = 'viewprod.html'
+}
+}
+
+form.addEventListener("submit", submitForm);
